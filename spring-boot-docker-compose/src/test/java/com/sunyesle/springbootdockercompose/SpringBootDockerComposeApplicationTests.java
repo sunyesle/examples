401d357c599848eb322222ac3c14d6ad923bd21d
sunyesle <sunyesle@gmail.com>
2024-05-15 19:49:02 +0900

    spring-boot-docker-compose init

---
+++ spring-boot-docker-compose/src/test/java/com/sunyesle/springbootdockercompose/SpringBootDockerComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunyesle.springbootdockercompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerComposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
