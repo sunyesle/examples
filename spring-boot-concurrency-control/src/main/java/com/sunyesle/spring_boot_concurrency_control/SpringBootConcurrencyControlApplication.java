b09238f3d00631f8369ede0489cad22ed3494ded
sunyesle <sunyesle@gmail.com>
2024-06-17 22:40:30 +0900

    [spring-boot-concurrency-control] init

---
+++ spring-boot-concurrency-control/src/main/java/com/sunyesle/spring_boot_concurrency_control/SpringBootConcurrencyControlApplication.java
@@ -0,0 +1,13 @@
+package com.sunyesle.spring_boot_concurrency_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConcurrencyControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConcurrencyControlApplication.class, args);
+	}
+
+}
---
+++ spring-boot-concurrency-control/src/test/java/com/sunyesle/spring_boot_concurrency_control/SpringBootConcurrencyControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunyesle.spring_boot_concurrency_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConcurrencyControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
