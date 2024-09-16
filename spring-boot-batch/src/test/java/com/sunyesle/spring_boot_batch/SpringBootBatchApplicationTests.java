de8497d1a0e50bd4fbaa87cdf7c4d7e2abc5cfff
sunyesle <sunyesle@gmail.com>
2024-09-16 22:47:55 +0900

    [spring-boot-batch] init

---
+++ spring-boot-batch/src/test/java/com/sunyesle/spring_boot_batch/SpringBootBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunyesle.spring_boot_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
