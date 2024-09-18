ad9a4597de9119e288a42fbc4c56d52cbd0a88bd
sunyesle <sunyesle@gmail.com>
2024-09-18 21:13:52 +0900

    [spring-boot-batch] Chunk

--- spring-boot-batch/src/main/java/com/sunyesle/spring_boot_batch/job/SimpleJobConfig.java
+++ spring-boot-batch/src/main/java/com/sunyesle/spring_boot_batch/job/SimpleJobConfig.java
@@ -7,6 +7,7 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.*;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,9 +18,10 @@ import org.springframework.transaction.PlatformTransactionManager;
 public class SimpleJobConfig {
 
     @Bean
-    public Job simpleJob(JobRepository jobRepository, Step taskletStep) {
+    public Job simpleJob(JobRepository jobRepository, Step taskletStep, Step chunkStep) {
         return new JobBuilder("simpleJob", jobRepository)
                 .start(taskletStep)
+                .next(chunkStep)
                 .build();
     }
 
@@ -36,5 +38,42 @@ public class SimpleJobConfig {
             log.info("Running tasklet step");
             return RepeatStatus.FINISHED;
         };
+    }
+
+    @Bean
+    public Step chunkStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("chunkStep", jobRepository)
+                .<String, String>chunk(1, transactionManager)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .build();
+    }
+
+    @Bean
+    public ItemReader<String> reader() {
+        return new ItemReader<String>() {
+            private int alreadyRead = 0;
+
+            @Override
+            public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+                if (alreadyRead < 5) {
+                    alreadyRead++;
+                    return "Hello, Spring Batch!";
+                } else {
+                    return null;
+                }
+            }
+        };
+    }
+
+    @Bean
+    public ItemProcessor<String, String> processor() {
+        return String::toUpperCase;
+    }
+
+    @Bean
+    public ItemWriter<String> writer() {
+        return item -> item.forEach(log::info);
     }
 }
