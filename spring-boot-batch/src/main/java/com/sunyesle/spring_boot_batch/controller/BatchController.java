415b09f16503acc5bfe52395f203ed7758087fed
sunyesle <sunyesle@gmail.com>
2024-09-26 22:07:17 +0900

    [spring-boot-batch] 배치 실행 컨트롤러 추가

---
+++ spring-boot-batch/src/main/java/com/sunyesle/spring_boot_batch/controller/BatchController.java
@@ -0,0 +1,36 @@
+package com.sunyesle.spring_boot_batch.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.NoSuchJobException;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/batch")
+@RequiredArgsConstructor
+public class BatchController {
+    private final JobLauncher jobLauncher;
+    private final JobRegistry jobRegistry;
+
+    @GetMapping("/my-job")
+    public String myJobApi(@RequestParam String date) throws NoSuchJobException, JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("date", date)
+                .toJobParameters();
+
+        jobLauncher.run(jobRegistry.getJob("myJob"), jobParameters);
+
+        return "ok";
+    }
+
+}
