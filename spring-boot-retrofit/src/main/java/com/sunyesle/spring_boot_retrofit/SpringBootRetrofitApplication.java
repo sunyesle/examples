60e3ddad1e2255b242c7489423c62add4e8dc101
sunyesle <sunyesle@gmail.com>
2024-10-30 21:44:03 +0900

    [spring-boot-retrofit] init

---
+++ spring-boot-retrofit/src/main/java/com/sunyesle/spring_boot_retrofit/SpringBootRetrofitApplication.java
@@ -0,0 +1,13 @@
+package com.sunyesle.spring_boot_retrofit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRetrofitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRetrofitApplication.class, args);
+	}
+
+}
---
+++ spring-boot-retrofit/src/test/java/com/sunyesle/spring_boot_retrofit/SpringBootRetrofitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunyesle.spring_boot_retrofit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRetrofitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
