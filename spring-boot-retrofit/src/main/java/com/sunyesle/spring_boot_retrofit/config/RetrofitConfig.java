8142a631fc7b4cbbfdeef86ed5013dbec09f27f5
sunyesle <sunyesle@gmail.com>
2024-10-31 20:26:19 +0900

    [spring-boot-retrofit] Converter를 Jackson으로 변경

--- spring-boot-retrofit/src/main/java/com/sunyesle/spring_boot_retrofit/config/RetrofitConfig.java
+++ spring-boot-retrofit/src/main/java/com/sunyesle/spring_boot_retrofit/config/RetrofitConfig.java
@@ -1,11 +1,14 @@
 package com.sunyesle.spring_boot_retrofit.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.sunyesle.spring_boot_retrofit.user.UserAPI;
 import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.jackson.JacksonConverterFactory;
 
 import java.util.concurrent.TimeUnit;
 
@@ -24,9 +27,13 @@ public class RetrofitConfig {
 
     @Bean
     public Retrofit retrofit(OkHttpClient client) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+
         String baseURL = BASE_URL + "api/";
         return new Retrofit.Builder().baseUrl(baseURL)
-                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(JacksonConverterFactory.create(objectMapper))
                 .client(client)
                 .build();
     }
--- spring-boot-retrofit/src/main/java/com/sunyesle/spring_boot_retrofit/user/UserResponse.java
+++ spring-boot-retrofit/src/main/java/com/sunyesle/spring_boot_retrofit/user/UserResponse.java
@@ -1,6 +1,5 @@
 package com.sunyesle.spring_boot_retrofit.user;
 
-import com.google.gson.annotations.SerializedName;
 import lombok.Getter;
 
 @Getter
@@ -12,13 +11,8 @@ public class UserResponse {
     public static class User {
         private Integer id;
         private String email;
-
-        @SerializedName("first_name")
         private String firstName;
-
-        @SerializedName("last_name")
         private String lastName;
-
         private String avatar;
     }
 
