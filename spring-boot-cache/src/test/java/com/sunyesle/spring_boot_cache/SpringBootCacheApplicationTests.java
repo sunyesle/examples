58e62fa894d6fa083468b69bdf52b2387f627ce8
sunyesle <sunyesle@gmail.com>
2024-11-10 17:37:56 +0900

    [spring-boot-cache] init

---
+++ spring-boot-cache/src/test/java/com/sunyesle/spring_boot_cache/SpringBootCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunyesle.spring_boot_cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
