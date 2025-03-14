d2fad2c3db01a14ead6ac4aa41085c4bf527aef7
sunyesle <sunyesle@gmail.com>
2025-03-14 22:46:50 +0900

    [spring-boot-logging] init

---
+++ spring_boot_logging/src/test/java/com/sunyesle/spring_boot_logging/SpringBootLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunyesle.spring_boot_logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
