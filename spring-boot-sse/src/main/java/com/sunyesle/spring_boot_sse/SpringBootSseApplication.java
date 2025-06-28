2209596edea2be6318f7b5e3c014f99e61be4527
sunyesle <sunyesle@gmail.com>
2025-06-28 22:20:57 +0900

    [spring-boot-sse] init

---
+++ spring-boot-sse/src/main/java/com/sunyesle/spring_boot_sse/SpringBootSseApplication.java
@@ -0,0 +1,13 @@
+package com.sunyesle.spring_boot_sse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSseApplication.class, args);
+	}
+
+}
