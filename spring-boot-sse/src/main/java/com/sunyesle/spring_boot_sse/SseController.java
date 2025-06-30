761f6caab866ce8f8191ee36711680637e379bd4
sunyesle <sunyesle@gmail.com>
2025-06-30 22:32:59 +0900

    [spring-boot-sse] 요청 로깅 추가

--- spring-boot-sse/src/main/java/com/sunyesle/spring_boot_sse/SseController.java
+++ spring-boot-sse/src/main/java/com/sunyesle/spring_boot_sse/SseController.java
@@ -1,10 +1,12 @@
 package com.sunyesle.spring_boot_sse;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/sse")
 @RequiredArgsConstructor
@@ -13,16 +15,19 @@ public class SseController {
 
     @GetMapping(value = "/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter connect(@RequestParam String userId) {
+        log.info("connect - userId={}", userId);
         return sseService.connect(userId);
     }
 
     @PostMapping("/broadcast")
     public void broadcast(@RequestBody SseBroadcastRequest request) {
+        log.info("broadcast - {}", request);
         sseService.broadcast(request);
     }
 
     @PostMapping("/send")
     public void send(@RequestBody SseRequest request) {
+        log.info("send - {}", request);
         sseService.send(request);
     }
 }
