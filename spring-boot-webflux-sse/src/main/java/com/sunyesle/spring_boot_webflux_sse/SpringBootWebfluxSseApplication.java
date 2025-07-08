9258fc5c5590685e5e8feb8227bf44f7115a33fe
sunyesle <sunyesle@gmail.com>
2025-07-08 20:05:48 +0900

    [spring-boot-webflux-sse] init

---
+++ spring-boot-webflux-sse/src/main/java/com/sunyesle/spring_boot_webflux_sse/SpringBootWebfluxSseApplication.java
@@ -0,0 +1,13 @@
+package com.sunyesle.spring_boot_webflux_sse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxSseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebfluxSseApplication.class, args);
+	}
+
+}
---
+++ spring-boot-webflux-sse/src/test/java/com/sunyesle/spring_boot_webflux_sse/SpringBootWebfluxSseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunyesle.spring_boot_webflux_sse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxSseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
