b0371040bac0d27f2a71b83b2122dcaf306d6651
sunyesle <sunyesle@gmail.com>
2025-12-15 20:09:16 +0900

    [spring-boot-api-versioning] init

---
+++ spring-boot-api-versioning/src/main/java/com/sunyesle/spring_boot_api_versioning/SpringBootApiVersioningApplication.java
@@ -0,0 +1,13 @@
+package com.sunyesle.spring_boot_api_versioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiVersioningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiVersioningApplication.class, args);
+	}
+
+}
---
+++ spring-boot-api-versioning/src/test/java/com/sunyesle/spring_boot_api_versioning/SpringBootApiVersioningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunyesle.spring_boot_api_versioning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiVersioningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
