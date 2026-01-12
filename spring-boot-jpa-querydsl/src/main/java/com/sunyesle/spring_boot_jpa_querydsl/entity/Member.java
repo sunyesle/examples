2023c79098cd6f20cffb66cb5e1fd9707b78f1bd
sunyesle <sunyesle@gmail.com>
2026-01-12 22:39:32 +0900

    [spring-boot-jpa-querydsl] Entity 추가

---
+++ spring-boot-jpa-querydsl/src/main/java/com/sunyesle/spring_boot_jpa_querydsl/entity/Member.java
@@ -0,0 +1,32 @@
+package com.sunyesle.spring_boot_jpa_querydsl.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "member")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(exclude = {"team"})
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+    private Integer age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String name, Integer age, Team team) {
+        this.name = name;
+        this.age = age;
+        this.team = team;
+    }
+}
---
+++ spring-boot-jpa-querydsl/src/main/java/com/sunyesle/spring_boot_jpa_querydsl/entity/Team.java
@@ -0,0 +1,31 @@
+package com.sunyesle.spring_boot_jpa_querydsl.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "team")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(exclude = {"members"})
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
