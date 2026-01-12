5efcedba7b0612bb927c3ac5d7c61f696bd3d4ab
sunyesle <sunyesle@gmail.com>
2026-01-12 22:16:14 +0900

    [spring-boot-jpa-querydsl] init

---
+++ spring-boot-jpa-querydsl/src/main/java/com/sunyesle/spring_boot_jpa_querydsl/SpringBootJpaQuerydslApplication.java
@@ -0,0 +1,13 @@
+package com.sunyesle.spring_boot_jpa_querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaQuerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaQuerydslApplication.class, args);
+	}
+
+}
---
+++ spring-boot-jpa-querydsl/src/test/java/com/sunyesle/spring_boot_jpa_querydsl/SpringBootJpaQuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunyesle.spring_boot_jpa_querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaQuerydslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
