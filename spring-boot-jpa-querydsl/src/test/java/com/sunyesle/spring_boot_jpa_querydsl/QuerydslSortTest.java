ef4817939752f9782dd38ab291e0e8a322c9333b
sunyesle <sunyesle@gmail.com>
2026-01-13 20:51:09 +0900

    [spring-boot-jpa-querydsl] 정렬

---
+++ spring-boot-jpa-querydsl/src/test/java/com/sunyesle/spring_boot_jpa_querydsl/QuerydslSortTest.java
@@ -0,0 +1,48 @@
+package com.sunyesle.spring_boot_jpa_querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sunyesle.spring_boot_jpa_querydsl.config.DataJpaQuerydslTest;
+import com.sunyesle.spring_boot_jpa_querydsl.entity.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static com.sunyesle.spring_boot_jpa_querydsl.entity.QMember.member;
+
+@DataJpaQuerydslTest
+class QuerydslSortTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Autowired
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    void setup() {
+        em.persist(new Member("member1", 10, null));
+        em.persist(new Member("member2", 20, null));
+        em.persist(new Member(null, 20, null));
+        em.persist(new Member("member3", 30, null));
+        em.persist(new Member("member4", 40, null));
+    }
+
+    @Test
+    void sort() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .orderBy(
+                        member.age.desc(),
+                        member.name.asc().nullsFirst()
+                        // asc(), desc()
+                        // nullsFirst(), nullsLast()
+                )
+                .fetch();
+
+        System.out.println(result);
+    }
+}
