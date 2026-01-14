8f329e7095c5ebbbcd957708bcc187bca272effb
sunyesle <sunyesle@gmail.com>
2026-01-14 22:48:15 +0900

    [spring-boot-jpa-querydsl] 집계함수, 그룹화

---
+++ spring-boot-jpa-querydsl/src/test/java/com/sunyesle/spring_boot_jpa_querydsl/QuerydslAggregateTest.java
@@ -0,0 +1,75 @@
+package com.sunyesle.spring_boot_jpa_querydsl;
+
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sunyesle.spring_boot_jpa_querydsl.config.DataJpaQuerydslTest;
+import com.sunyesle.spring_boot_jpa_querydsl.entity.Member;
+import com.sunyesle.spring_boot_jpa_querydsl.entity.Team;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static com.sunyesle.spring_boot_jpa_querydsl.entity.QMember.member;
+import static com.sunyesle.spring_boot_jpa_querydsl.entity.QTeam.team;
+
+@DataJpaQuerydslTest
+class QuerydslAggregateTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Autowired
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    void setup() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    void aggregation() {
+        List<Tuple> result = queryFactory
+                .select(member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min())
+                .from(member)
+                .fetch();
+
+        System.out.println(result);
+    }
+
+    @Test
+    void groupBy_having() {
+        List<Tuple> result = queryFactory
+                .select(member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min())
+                .from(member)
+                .join(member.team, team)
+                .groupBy(team.name)
+                .having(member.age.avg().gt(30))
+                .fetch();
+
+        System.out.println(result);
+    }
+}
