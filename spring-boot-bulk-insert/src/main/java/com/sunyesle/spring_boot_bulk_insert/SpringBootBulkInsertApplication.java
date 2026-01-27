5e2dd42f36856541afc756b4fda96a8d929be670
sunyesle <sunyesle@gmail.com>
2026-01-27 20:24:55 +0900

    [spring-boot-bulk-insert] init

---
+++ spring-boot-bulk-insert/src/main/java/com/sunyesle/spring_boot_bulk_insert/SpringBootBulkInsertApplication.java
@@ -0,0 +1,13 @@
+package com.sunyesle.spring_boot_bulk_insert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBulkInsertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBulkInsertApplication.class, args);
+	}
+
+}
