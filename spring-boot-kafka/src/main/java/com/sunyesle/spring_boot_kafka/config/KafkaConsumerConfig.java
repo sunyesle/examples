53dad4502d26d0dacab75a9027c14090f2a1755d
sunyesle <sunyesle@gmail.com>
2026-05-07 21:13:10 +0900

    [spring-boot-kafka] Java Config

---
+++ spring-boot-kafka/src/main/java/com/sunyesle/spring_boot_kafka/config/KafkaConsumerConfig.java
@@ -0,0 +1,35 @@
+package com.sunyesle.spring_boot_kafka.config;
+
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//Kafka Consumer Java Config 예시
+//@Configuration
+public class KafkaConsumerConfig {
+
+    private static final String BOOTSTRAP_SERVERS = "localhost:19092";
+    private static final String SCHEMA_REGISTRY_URL = "http://localhost:18081";
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        props.put("schema.registry.url", SCHEMA_REGISTRY_URL);
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
