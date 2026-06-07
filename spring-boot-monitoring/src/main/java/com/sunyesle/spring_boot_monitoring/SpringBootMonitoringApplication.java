b366883ea8f659c4776ef4bfc654c7f73b8aef72
sunyesle <sunyesle@gmail.com>
2026-06-07 19:59:33 +0900

    [spring-boot-monitoring] init

---
+++ spring-boot-monitoring/src/main/java/com/sunyesle/spring_boot_monitoring/SpringBootMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.sunyesle.spring_boot_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMonitoringApplication.class, args);
+	}
+
+}
---
+++ spring-boot-monitoring/src/test/java/com/sunyesle/spring_boot_monitoring/SpringBootMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunyesle.spring_boot_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
